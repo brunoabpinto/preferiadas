9028841ebb4e91a36446d05905112bc89f86aa76
Bruno Pinto <bruno.pinto@inboxagency.com>
2020-04-09 11:59:18 +0100

    Xavier coin

--- resources/views/coin.blade.php
+++ resources/views/coin.blade.php
@@ -5,8 +5,20 @@
 <div class="container text-center mt-5">
     <h2>COIN</h2>
     <div class="coin mx-auto">
-        <div class="face side-a">H</div>
-        <div class="face side-b">T</div>
+        <div class="face side-a">
+            @if(request('coin') == 'xavier')
+                <img src="{{ asset('img/heads.png') }}">
+            @else
+                H
+            @endif
+        </div>
+        <div class="face side-b">
+            @if(request('coin') == 'xavier')
+                <img src="{{ asset('img/tails.png') }}">
+            @else
+                T
+            @endif
+        </div>
     </div>
 
     <button class="btn mt-3" type="submit" onclick="flipCoin()">Flip</button>
