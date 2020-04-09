f6a0a86b4c2d4cb88360ba52ee2555b15243b112
Bruno Pinto <bruno.pinto@inboxagency.com>
2020-04-09 01:50:08 +0100

    styling changes

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="btn-group footer-buttons" role="group">
-    <a type="button" class="btn {{ request()->path() == 'words' ? 'active' : '' }}" href="/words">Words</button>
-    <a type="button" class="btn {{ request()->path() == 'theme' ? 'active' : '' }}" href="/theme">Theme</button>
-    <a type="button" class="btn {{ request()->path() == 'coin' ? 'active' : '' }}" href="/coin">Coin</button>
-    <a type="button" class="btn {{ request()->path() == 'dice' ? 'active' : '' }}" href="/dice">Dice</a>
+    <a type="button" class="btn {{ request()->path() == 'words' ? 'active' : '' }}" href="/words">words</button>
+    <a type="button" class="btn {{ request()->path() == 'theme' ? 'active' : '' }}" href="/theme">theme</button>
+    <a type="button" class="btn {{ request()->path() == 'coin' ? 'active' : '' }}" href="/coin">coin</button>
+    <a type="button" class="btn {{ request()->path() == 'dice' ? 'active' : '' }}" href="/dice">dice</a>
 </div>
