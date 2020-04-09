ee466e86e678298b7051fc315fedd21aefa1dd4c
Bruno Pinto <bruno.pinto@inboxagency.com>
2020-04-09 11:27:25 +0100

    theme color

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="theme-color" content="#e74c3c" />
     <title>Preferíadas</title>
     <link rel="shortcut icon" href="{{ asset('img/logo.png') }}" />
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
